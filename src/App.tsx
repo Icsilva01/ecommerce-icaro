8053786de2c16c3db3580a03098c2fddb0df306a
Icaro Silva <107263783+Icsilva01@users.noreply.github.com>
2024-04-18 21:05:20 -0300

    feat: add block in route home

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { ForgetPassword, Home, Login, SignUp } from "./presentation";
 import { AuthProvider } from "./provider/AuthProvider";
+import { useAuth } from "./utils/useAuth";
 
 function App() {
+  const { user } = useAuth();
+
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="*" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          {user ? (
+            <Route path="/" element={<Home />} />
+          ) : (
+            <Route path="/" element={<Login />} />
+          )}
+          {/* <Route path="/login" element={<Login />} /> */}
           <Route path="/forget-password" element={<ForgetPassword />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
